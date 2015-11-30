33ec24c234620eeca061214089efd7b0e0004325
Florencia <flor9477@gmail.com>
2015-11-30 15:05:06 -0300

    comit

---
+++ index.php
@@ -0,0 +1,233 @@
+<?php 
+ini_set('display_errors',1);
+ini_set('display_startup_errors',1);
+error_reporting(-1);
+
+require 'vendor/autoload.php';
+require 'Models/User.php';
+
+$app = new \Slim\Slim();
+
+$app->config('databases', [
+    'default' => [
+        'driver'    => 'mysql',
+        'host'      => 'us-cdbr-iron-east-03.cleardb.net',
+        'database'  => 'heroku_907293980da2c4e',
+        'username'  => 'b0b1728ef96619',
+        'password'  => 'd65a478e',
+        'charset'   => 'utf8',
+        'collation' => 'utf8_general_ci',
+        'prefix'    => ''
+    ]
+]);
+
+
+
+/*$app->config('databases', [
+    'default' => [
+        'driver'    => 'mysql',
+        'host'      => 'localhost',
+        'database'  => 'glucosa',
+        'username'  => 'codipa',
+        'password'  => 'codipa',
+        'charset'   => 'utf8',
+        'collation' => 'utf8_general_ci',
+        'prefix'    => ''
+    ]
+]);*/
+
+
+$app->add(new Zeuxisoo\Laravel\Database\Eloquent\ModelMiddleware);
+
+$app->view(new \JsonApiView());
+$app->add(new \JsonApiMiddleware());
+$app->add(new \Slim\Middleware\ContentTypes());
+
+$app->options('/(:name+)', function() use ($app) {
+    $app->render(200,array('msg' => 'CODIPA'));
+});
+
+$app->get('/', function () use ($app) {
+	$app->render(200,array('msg' => 'CODIPA'));
+});
+
+//Mostrar
+
+
+$app->get('/usuarios', function () use ($app) {
+	$db = $app->db->getConnection();
+	$usuarios = $db->table('usuarios')->select('id', 'name')->get();
+	$app->render(200,array('data' => $usuarios));
+});
+
+
+//Registrousua 
+
+$app->post('/login', function () use ($app) {
+	$input = $app->request->getBody();
+
+	$email = $input['email'];
+	if(empty($email)){
+		$app->render(500,array(
+			'error' => TRUE,
+            'msg'   => 'Se requiere el Email',
+        ));
+	}
+	$password = $input['password'];
+	if(empty($password)){
+		$app->render(500,array(
+			'error' => TRUE,
+            'msg'   => 'Se requiere la Contraseña',
+        ));
+	}
+	$db = $app->db->getConnection();
+	$usuarios = $db->table('usuarios')->select()->where('email', $email)->first();
+	if(empty($user)){
+		$app->render(500,array(
+			'error' => TRUE,
+            'msg'   => 'El usuario no existe',
+        ));
+	}
+	if($usuarios->password != $password){
+		$app->render(500,array(
+			'error' => TRUE,
+            'msg'   => 'La password no coincide',
+        ));
+	}
+
+	$token = simple_encrypt($usuarios->id, $app->enc_key);
+
+	$app->render(200,array('token' => $token));
+});
+
+$app->get('/me', function () use ($app) {
+
+	$token = $app->request->headers->get('auth-token');
+
+	if(empty($token)){
+		$app->render(500,array(
+			'error' => TRUE,
+            'msg'   => 'Not logged1',
+        ));
+	}
+	
+	$id_user_token = simple_decrypt($token, $app->enc_key);
+
+	$usuarios = usuarios::find($id_user_token);
+	if(empty($usuarios)){
+		$app->render(500,array(
+			'error' => TRUE,
+            'msg'   => 'Not logged2',
+        ));
+	}
+	$app->render(200,array('data' => $usuarios->toArray()));
+});
+
+
+//logout
+$app->get('/login', function () use ($app) {
+	$db = $app->db->getConnection();
+	$usuarios = $db->table('usuarios')->select('id', 'name')->get();
+
+	$app->render(200,array('data' => $users));
+});
+
+$app->post('/usuarios', function () use ($app) {
+	$input = $app->request->getBody();
+
+	$name = $input['name'];
+	if(empty($name)){
+		$app->render(500,array(
+			'error' => TRUE,
+            'msg'   => 'name is required',
+        ));
+	}
+	$password = $input['password'];
+	if(empty($password)){
+		$app->render(500,array(
+			'error' => TRUE,
+            'msg'   => 'password is required',
+        ));
+	}
+	$email = $input['email'];
+	if(empty($email)){
+		$app->render(500,array(
+			'error' => TRUE,
+            'msg'   => 'email is required',
+        ));
+	}
+	
+    $user = new User();
+    $user->name = $name;
+    $user->password = $password;
+    $user->email = $email;
+    $user->save();
+
+    $app->render(200,array('data' => $user->toArray()));
+});
+
+$app->put('/usuarios/:id', function ($id) use ($app) {
+	$input = $app->request->getBody();
+	
+	$name = $input['name'];
+	if(empty($name)){
+		$app->render(500,array(
+			'error' => TRUE,
+            'msg'   => 'name is required',
+        ));
+	}
+	$password = $input['password'];
+	if(empty($password)){
+		$app->render(500,array(
+			'error' => TRUE,
+            'msg'   => 'password is required',
+        ));
+	}
+	$email = $input['email'];
+	if(empty($email)){
+		$app->render(500,array(
+			'error' => TRUE,
+            'msg'   => 'email is required',
+        ));
+	}
+
+	$user = User::find($id);
+	if(empty($user)){
+		$app->render(404,array(
+			'error' => TRUE,
+            'msg'   => 'user not found',
+        ));
+	}
+    $user->name = $name;
+    $user->password = $password;
+    $user->email = $email;
+    $user->save();
+    $app->render(200,array('data' => $user->toArray()));
+});
+
+$app->get('/usuarios/:id', function ($id) use ($app) {
+	$user = User::find($id);
+	if(empty($user)){
+		$app->render(404,array(
+			'error' => TRUE,
+            'msg'   => 'user not found',
+        ));
+	}
+	$app->render(200,array('data' => $user->toArray()));
+});
+
+$app->delete('/usuarios/:id', function ($id) use ($app) {
+	$user = User::find($id);
+	if(empty($user)){
+		$app->render(404,array(
+			'error' => TRUE,
+            'msg'   => 'user not found',
+        ));
+	}
+
+	$user->delete();
+	$app->render(200);
+});
+
+$app->run();
+?>
